cca21c170fc5eb49a51f38b0970b2451ed34218a
Rasel5267 <mahadiul09@gmail.com>
2023-05-30 03:41:59 +0600

    [done] create user controller and routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application } from 'express'
 import cors from 'cors'
+import { UserRoute } from './app/modules/users/users.routes'
 
 const app: Application = express()
 
@@ -8,5 +9,7 @@ app.use(cors())
 //parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use('/api/v1/user', UserRoute)
 
 export default app
--- src/app/modules/users/users.model.ts
+++ src/app/modules/users/users.model.ts
@@ -21,6 +21,7 @@ const userSchema = new Schema<IUser>(
   },
   {
     timestamps: true,
+    versionKey: false,
   }
 )
 
